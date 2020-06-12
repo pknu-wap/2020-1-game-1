89919880deeb590b7057eb9bf21bbf350a9a3a32
haeinsa <wo4014@gmail.com>
2020-06-12 20:04:47 +0900

    유닛 자동이동, 공격 컴포넌트 구현

---
+++ Assets/Scenes/Scripts/SeungMin/UnitAttackComponent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitAttackComponent : MonoBehaviour
+{
+    private GameObject player;
+    private UnitAttackComponent playerStatus;
+    [SerializeField]
+    public float healthPoint = 100f;
+    [SerializeField]
+    private float attackSpeed = 1f;
+    [SerializeField]
+    private float attackDamage = 10f;
+    [SerializeField]
+    public float attackRange = 2f;
+
+    public static WaitForSeconds statusCheckPeriod = new WaitForSeconds(0.2f);
+    public static WaitForSeconds waitAttackSpeed;
+    private IEnumerator AttackPlayer()
+    {
+        while (true)
+        {
+            CheckDead();
+            if ((transform.position - player.transform.position).magnitude <= attackRange)
+            {
+                playerStatus.healthPoint = -attackDamage;
+                yield return waitAttackSpeed;
+            }
+            else
+                yield return statusCheckPeriod;
+        }
+    }
+    private IEnumerator CheckDead()
+    {
+        while (true) {
+            if (healthPoint <= 0)
+            {
+                Destroy(gameObject);
+                StopAllCoroutines();
+            }
+
+            yield return statusCheckPeriod;
+            
+        }
+    }
+
+    private void OnEnable()
+    {
+        player = GameObject.Find("Player");
+        playerStatus = player.GetComponent<UnitAttackComponent>();
+        waitAttackSpeed = new WaitForSeconds(attackSpeed);
+        StartCoroutine(AttackPlayer());
+        StartCoroutine(CheckDead());
+    }
+
+}
---
+++ Assets/Scenes/Scripts/SeungMin/UnitMovementComponent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class UnitMovementComponent : MonoBehaviour
+{
+    private GameObject player;
+    private UnitAttackComponent attackComponent;
+    private Vector3 attackPosition;
+    [SerializeField]
+    private float speed=5f;
+    [SerializeField]
+    private float recognitionDistance = 30f;
+    private NavMeshAgent navMeshAgent;
+
+    private IEnumerator ChasePlayer()
+    {
+        while (true)
+        {
+            if((transform.position - player.transform.position).magnitude <= recognitionDistance)
+            {
+                attackPosition = player.transform.position + (transform.position - player.transform.position).normalized * attackComponent.attackRange;
+                if (navMeshAgent.SetDestination(attackPosition))
+                    navMeshAgent.isStopped = false;
+                else
+                   navMeshAgent.isStopped =  true;
+            }
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        player = GameObject.Find("Player");
+        attackComponent = GetComponent<UnitAttackComponent>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = speed;
+        navMeshAgent.isStopped = true;
+    }
+}

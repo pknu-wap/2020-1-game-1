fc58dc4d8fe174065356f28cd93744d30263ae62
dohun <dohun31@naver.com>
2020-06-14 18:41:55 +0900

    포탈

---
+++ dh/Assets/move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Rigidbody rigid;
+    void Start() {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(Vector3.up * 25, ForceMode.Impulse);
+        }
+        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        rigid.AddForce(vec, ForceMode.Impulse);
+    }
+}
---
+++ dh/Assets/potal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potal : MonoBehaviour
+{
+    public Transform TranslatePosition;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Transform ParentTransform = other.transform;
+
+            ParentTransform.position = TranslatePosition.position;
+            ParentTransform.rotation = TranslatePosition.rotation;
+
+        }
+    }
+
+}
